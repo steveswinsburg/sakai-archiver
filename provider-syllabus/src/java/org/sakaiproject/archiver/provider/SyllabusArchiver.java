33d5652f4775ad55017c7222fe73980b263ab3b0
Amelia Davis <amiedav@gmail.com>
2017-05-18 10:54:01 +1000

    #14 Fixes as per PR comments
    Remove archiver-api version from pom
    Log error when getting attachments, and continue
    Save content separately as html rather than json

--- provider-syllabus/src/java/org/sakaiproject/archiver/provider/SyllabusArchiver.java
+++ provider-syllabus/src/java/org/sakaiproject/archiver/provider/SyllabusArchiver.java
@@ -63,9 +63,16 @@ public class SyllabusArchiver implements Archiveable {
 		for (SyllabusData syllabus : syllabusSet) {
 			log.debug("Collecting " + syllabus.getTitle());
 			archiveItem.setTitle(syllabus.getTitle());
-			archiveItem.setData(syllabus.getAsset());
 			archiveItem.setStartDate(syllabus.getStartDate());
 			archiveItem.setEndDate(syllabus.getEndDate());
+
+			String jsonArchiveItem = Jsonifier.toJson(archiveItem);
+			log.debug("Archive item metadata: " + jsonArchiveItem);
+			this.archiverService.archiveContent(archiveId, siteId, "sakai.syllabus", jsonArchiveItem.getBytes(), archiveItem.getTitle() + " (metadata).json");
+			
+			String archiveData = syllabus.getAsset();
+			log.debug("Archive item content: " + archiveData);
+			this.archiverService.archiveContent(archiveId, siteId, "sakai.syllabus", archiveData.getBytes(), archiveItem.getTitle() + " (content).html");
 
 			//get the attachments
 			Set<SyllabusAttachment> syllabusAttachments = this.syllabusManager.getSyllabusAttachmentsForSyllabusData(syllabus);
@@ -74,29 +81,22 @@ public class SyllabusArchiver implements Archiveable {
 				byte[] syllabusAttachmentBytes;
 				try {
 					syllabusAttachmentBytes = this.contentHostingService.getResource(syllabusAttachment.getAttachmentId()).getContent();
-					this.archiverService.archiveContent(archiveId, siteId, "sakai.syllabus", syllabusAttachmentBytes, syllabusAttachment.getName(), syllabus.getTitle());
-					log.debug("Attachment:  " +syllabusAttachment.getName() + " Sub-directory: " + syllabus.getTitle());
+					this.archiverService.archiveContent(archiveId, siteId, "sakai.syllabus", syllabusAttachmentBytes, syllabusAttachment.getName(), syllabus.getTitle() + " (attachments)");
 				} catch (ServerOverloadException | PermissionException | IdUnusedException | TypeException e) {
-					e.printStackTrace();
+					log.error("Error getting syllabus attachment " + syllabusAttachment.getName() + " in syllabus " + syllabus.getTitle());
+					continue;
 				}
 			}
-			log.debug("JSON: " +Jsonifier.toJson(archiveItem));
-			this.archiverService.archiveContent(archiveId, siteId, "sakai.syllabus", Jsonifier.toJson(archiveItem).getBytes(), archiveItem.getTitle() + ".json");
 		}
-		
-
 	}
 	
 	/**
-	 * Simplified helper class to represent an individual syllabus item in a site
+	 * Simplified helper class to represent metadata for an individual syllabus item in a site
 	 */
 	public static class ArchiveItem {
 		
 		@Getter @Setter
 		private String title;
-		
-		@Getter @Setter
-		private String data;
 		
 		@Getter @Setter
 		private Date startDate;
