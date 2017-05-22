cc5f6b7ebc0c5e708b9e4cc22821ebc0b499af9c
Amelia Davis <amiedav@gmail.com>
2017-05-22 14:35:25 +1000

    #21 finished implementation of archiver for announcements
    Announcement metadata and body content are saved as separate files, with attachments in their own folder.

--- provider-announcements/src/java/org/sakaiproject/archiver/provider/AnnouncementsArchiver.java
+++ provider-announcements/src/java/org/sakaiproject/archiver/provider/AnnouncementsArchiver.java
@@ -1,15 +1,25 @@
 package org.sakaiproject.archiver.provider;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import org.sakaiproject.announcement.api.AnnouncementChannel;
+import org.sakaiproject.announcement.api.AnnouncementMessage;
 import org.sakaiproject.announcement.api.AnnouncementService;
 import org.sakaiproject.archiver.api.Archiveable;
 import org.sakaiproject.archiver.api.ArchiverRegistry;
 import org.sakaiproject.archiver.api.ArchiverService;
+import org.sakaiproject.archiver.util.Jsonifier;
+import org.sakaiproject.content.api.ContentHostingService;
+import org.sakaiproject.entity.api.Reference;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.exception.PermissionException;
+import org.sakaiproject.exception.ServerOverloadException;
+import org.sakaiproject.exception.TypeException;
+import org.sakaiproject.message.api.Message;
+import org.sakaiproject.site.api.SiteService;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,24 +45,67 @@ public class AnnouncementsArchiver implements Archiveable {
 	
 	@Setter 
 	private AnnouncementService announcementService;
+	
+	@Setter
+	private ContentHostingService contentHostingService;
 
 	@Override
 	public void archive(final String archiveId, final String siteId, final String toolId, final boolean includeStudentContent) {
 		
 		try {
-			AnnouncementChannel channel = announcementService.getAnnouncementChannel(announcementService.getAnnouncementReference(siteId).getId());
-			List messages = channel.getMessages(null, true);
 			
-//			for (Message message : (List<Message>) messages) {
-//				
-//			}
-			System.out.println(messages.toString());
-			this.archiverService.archiveContent(archiveId, siteId, toolId, messages.toString().getBytes(), "announcements-export.json");
+			// Get the announcements for this site
+			String channelRef = this.announcementService.channelReference(siteId, SiteService.MAIN_CONTAINER);
+			List<Message> announcements = new ArrayList<Message>();
+			announcements.addAll(this.announcementService.getMessages(channelRef, null, 0, true, false, false));
 			
-		} catch (IdUnusedException | PermissionException e) {
+			// Go through each announcement and save the data we are interested in
+			for (Message message : announcements) {
+				AnnouncementMessage announcement = (AnnouncementMessage) message;
+				
+				// Save the metadata for this announcement
+				ArchiveItem archiveItem = new ArchiveItem();
+				archiveItem.setTitle(announcement.getAnnouncementHeader().getSubject());
+				archiveItem.setCreatedBy(announcement.getHeader().getFrom().getDisplayName());
+				archiveItem.setCreatedOn(new Date(announcement.getHeader().getDate().getTime()));
+				String jsonArchiveItem = Jsonifier.toJson(archiveItem);
+				log.debug("Announcement metadata: " + jsonArchiveItem);
+				this.archiverService.archiveContent(archiveId, siteId, toolId, jsonArchiveItem.getBytes(), archiveItem.getTitle() + " (metadata).json");
+				
+				// Save the body of the announcement
+				String announcementBody = announcement.getBody();
+				log.debug("Announcement body: " + announcementBody);
+				this.archiverService.archiveContent(archiveId, siteId, toolId, announcementBody.getBytes(), archiveItem.getTitle() + " (content).html");
+				
+				// get the attachments
+				for (Reference attachment : announcement.getAnnouncementHeader().getAttachments()) {
+					byte[] attachmentBytes;
+					try {
+						attachmentBytes = this.contentHostingService.getResource(attachment.getId()).getContent();
+						this.archiverService.archiveContent(archiveId, siteId, toolId, attachmentBytes, 
+								attachment.getProperties().getPropertyFormatted(attachment.getProperties().getNamePropDisplayName()), 
+								archiveItem.getTitle() + " (attachments)");
+					} catch (ServerOverloadException | IdUnusedException | TypeException e) {
+						log.debug("Error getting attachment for announcement: " + archiveItem.getTitle());
+						continue;
+					}
+					
+				}
+			}
+		} catch (PermissionException e) {
 			log.debug("Failed to get announcements");
 		}
+	}
+	
+	/**
+	 * Simplified helper class to represent metadata for an individual announcement in a site
+	 */
+	private static class ArchiveItem {
 		
+		@Getter @Setter private String title;
+		@Getter @Setter private String createdBy;
+		@Getter @Setter private Date createdOn;
+		@Getter @Setter private String channel;
 	}
 
 }
