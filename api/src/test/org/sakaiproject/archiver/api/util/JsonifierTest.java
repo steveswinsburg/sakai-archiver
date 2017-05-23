112a30a85ca30a299b9ed783656363286906fbd6
Steve Swinsburg <steve.swinsburg@gmail.com>
2017-05-23 20:02:01 +1000

    Fix Jsonifier test for timezones

--- api/src/test/org/sakaiproject/archiver/api/util/JsonifierTest.java
+++ api/src/test/org/sakaiproject/archiver/api/util/JsonifierTest.java
@@ -2,12 +2,17 @@ package org.sakaiproject.archiver.api.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.sakaiproject.archiver.util.Jsonifier;
 
@@ -18,6 +23,19 @@ import lombok.Setter;
  * Test for the {@link Jsonifier} utility
  */
 public class JsonifierTest {
+
+	Date date1;
+	Date date2;
+	Date date3;
+	Date date4;
+
+	@Before
+	public void setup() {
+		this.date1 = getDate(4);
+		this.date2 = getDate(-6);
+		this.date3 = getDate(17);
+		this.date4 = getDate(-11);
+	}
 
 	@Test
 	public void should_serialiseToJson_when_simpleStringProvided() {
@@ -34,7 +52,8 @@ public class JsonifierTest {
 		testObject.setParam1("param1");
 		testObject.setParam2(2);
 		testObject.setParam4(false);
-		testObject.setParam3(new Date(888888888));
+
+		testObject.setParam3(this.date1);
 
 		final List<String> list = new ArrayList<>();
 		list.add("elem1");
@@ -42,9 +61,9 @@ public class JsonifierTest {
 		testObject.setList1(list);
 
 		final Map<Integer, Date> map1 = new LinkedHashMap<Integer, Date>();
-		map1.put(1, new Date(111111111));
-		map1.put(9, new Date(999999999));
-		map1.put(2, new Date(222222222));
+		map1.put(1, this.date2);
+		map1.put(9, this.date3);
+		map1.put(2, this.date4);
 		testObject.setMap1(map1);
 
 		final String result = Jsonifier.toJson(testObject);
@@ -86,17 +105,27 @@ public class JsonifierTest {
 		sb.append("{");
 		sb.append("\"param1\":\"param1\",");
 		sb.append("\"param2\":2,");
-		sb.append("\"param3\":\"Jan 11, 1970 4:54:48 PM\",");
+		sb.append("\"param3\":\"" + expectedDateSerialisation(this.date1) + "\",");
 		sb.append("\"param4\":false,");
 		sb.append("\"list1\":[\"elem1\",\"elem2\"],");
 		sb.append("\"map1\":{");
-		sb.append("\"1\":\"Jan 2, 1970 4:51:51 PM\",");
-		sb.append("\"9\":\"Jan 12, 1970 11:46:39 PM\",");
-		sb.append("\"2\":\"Jan 3, 1970 11:43:42 PM\"");
+		sb.append("\"1\":\"" + expectedDateSerialisation(this.date2) + "\",");
+		sb.append("\"9\":\"" + expectedDateSerialisation(this.date3) + "\",");
+		sb.append("\"2\":\"" + expectedDateSerialisation(this.date4) + "\"");
 		sb.append("}");
 		sb.append("}");
 		return sb.toString();
-		// {"param1":"param1","param2":2,"param3":"Jan 11, 1970 4:54:48 PM","param4":false,"list1":["elem1","elem2"],"map1":{"1":"Jan 2,
-		// 1970 4:51:51 PM","9":"Jan 12, 1970 11:46:39 PM","2":"Jan 3, 1970 11:43:42 PM"}}
+
+	}
+
+	private Date getDate(final int hourOffset) {
+		final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Australia/Canberra"));
+		calendar.add(Calendar.HOUR_OF_DAY, hourOffset);
+		return calendar.getTime();
+	}
+
+	private String expectedDateSerialisation(final Date date) {
+		final DateFormat dateFormat = new SimpleDateFormat("MMM dd, YYYY h:mm:ss a");
+		return dateFormat.format(date);
 	}
 }
