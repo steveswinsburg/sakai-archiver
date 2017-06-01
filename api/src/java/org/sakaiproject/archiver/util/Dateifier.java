8f507bda19cf6637bdf89b09b560b356c80e37d5
Steve Swinsburg <steve.swinsburg@gmail.com>
2017-06-02 07:52:13 +1000

    Add Dateifier helper

---
+++ api/src/java/org/sakaiproject/archiver/util/Dateifier.java
@@ -0,0 +1,44 @@
+package org.sakaiproject.archiver.util;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Utility for working with dates
+ */
+public class Dateifier {
+
+	/**
+	 * Format a date to ISO 8601 using default Locale
+	 *
+	 * @param date
+	 * @return string or null if date is null
+	 */
+	public static String toIso8601(final Date date) {
+		return toIso8601(date, Locale.getDefault());
+	}
+
+	/**
+	 * Format a date to ISO 8601 using specific Locale
+	 *
+	 * @param date
+	 * @param locale the locale to format the date to
+	 * @return string or null if date is null
+	 */
+	public static String toIso8601(final Date date, final Locale locale) {
+		if (date == null) {
+			return null;
+		}
+
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm'Z'")
+				.withLocale(locale)
+				.withZone(ZoneId.systemDefault());
+
+		final Instant instant = date.toInstant();
+		return formatter.format(instant);
+	}
+
+}
