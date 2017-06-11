d9f849a50a54c149df08b766fc8db1a4afc23fd8
Steve Swinsburg <steve.swinsburg@gmail.com>
2017-06-12 09:25:24 +1000

    #54 Let announcements export as HTML

--- provider-announcements/src/java/org/sakaiproject/archiver/provider/AnnouncementsArchiver.java
+++ provider-announcements/src/java/org/sakaiproject/archiver/provider/AnnouncementsArchiver.java
@@ -9,7 +9,7 @@ import org.sakaiproject.announcement.api.AnnouncementService;
 import org.sakaiproject.archiver.api.ArchiverRegistry;
 import org.sakaiproject.archiver.api.ArchiverService;
 import org.sakaiproject.archiver.spi.Archiveable;
-import org.sakaiproject.archiver.util.Jsonifier;
+import org.sakaiproject.archiver.util.Htmlifier;
 import org.sakaiproject.content.api.ContentHostingService;
 import org.sakaiproject.entity.api.Reference;
 import org.sakaiproject.exception.IdUnusedException;
@@ -42,88 +42,93 @@ public class AnnouncementsArchiver implements Archiveable {
 
 	@Setter
 	private ArchiverService archiverService;
-	
-	@Setter 
+
+	@Setter
 	private AnnouncementService announcementService;
-	
+
 	@Setter
 	private ContentHostingService contentHostingService;
 
 	@Override
 	public void archive(final String archiveId, final String siteId, final String toolId, final boolean includeStudentContent) {
-		
+
 		try {
-			
+
 			// Get the announcements for this site
-			String channelRef = this.announcementService.channelReference(siteId, SiteService.MAIN_CONTAINER);
-			List<Message> announcements = new ArrayList<Message>();
+			final String channelRef = this.announcementService.channelReference(siteId, SiteService.MAIN_CONTAINER);
+			final List<Message> announcements = new ArrayList<Message>();
 			announcements.addAll(this.announcementService.getMessages(channelRef, null, 0, true, false, false));
-			
+
 			// Go through each announcement and save the data we are interested in
-			for (Message message : announcements) {
-				AnnouncementMessage announcement = (AnnouncementMessage) message;
-				
+			for (final Message message : announcements) {
+				final AnnouncementMessage announcement = (AnnouncementMessage) message;
+
 				// Save this announcement
-				ArchiveItem archiveItem = createArchiveItem(announcement);
-				String jsonArchiveItem = Jsonifier.toJson(archiveItem);
-				log.debug("Announcement data: " + jsonArchiveItem);
-				this.archiverService.archiveContent(archiveId, siteId, toolId, jsonArchiveItem.getBytes(), archiveItem.getTitle() + ".json");
-				
+				final ArchiveItem archiveItem = createArchiveItem(announcement);
+				final String fileContents = Htmlifier.toHtml(archiveItem);
+				log.debug("Announcement data: " + fileContents);
+				this.archiverService.archiveContent(archiveId, siteId, toolId, fileContents.getBytes(), archiveItem.getTitle() + ".html");
+
 				// get the attachments
-				for (Reference attachment : announcement.getAnnouncementHeader().getAttachments()) {
+				for (final Reference attachment : announcement.getAnnouncementHeader().getAttachments()) {
 					byte[] attachmentBytes;
 					try {
 						attachmentBytes = this.contentHostingService.getResource(attachment.getId()).getContent();
-						this.archiverService.archiveContent(archiveId, siteId, toolId, attachmentBytes, 
-								attachment.getProperties().getPropertyFormatted(attachment.getProperties().getNamePropDisplayName()), 
+						this.archiverService.archiveContent(archiveId, siteId, toolId, attachmentBytes,
+								attachment.getProperties().getPropertyFormatted(attachment.getProperties().getNamePropDisplayName()),
 								archiveItem.getTitle() + " (attachments)");
 					} catch (ServerOverloadException | IdUnusedException | TypeException e) {
 						log.error("Error getting attachment for announcement: " + archiveItem.getTitle());
 						continue;
 					}
-					
+
 				}
 			}
-		} catch (PermissionException e) {
+		} catch (final PermissionException e) {
 			log.error("Failed to get announcements");
 		}
 	}
-	
+
 	/**
 	 * Build the ArchiveItem for an announcement
-	 * 
+	 *
 	 * @param announcement
 	 * @return the archive item to be saved
 	 */
-	private ArchiveItem createArchiveItem(AnnouncementMessage announcement) {
-		
-		ArchiveItem archiveItem = new ArchiveItem();
+	private ArchiveItem createArchiveItem(final AnnouncementMessage announcement) {
+
+		final ArchiveItem archiveItem = new ArchiveItem();
 		archiveItem.setTitle(announcement.getAnnouncementHeader().getSubject());
 		archiveItem.setCreatedBy(announcement.getHeader().getFrom().getDisplayName());
 		archiveItem.setCreatedOn(new Date(announcement.getHeader().getDate().getTime()));
-		archiveItem.setBody(announcement.getBody()); 
-		
+		archiveItem.setBody(announcement.getBody());
+
 		return archiveItem;
 	}
-	
+
 	/**
 	 * Simplified helper class to represent individual announcement in a site
 	 */
 	private class ArchiveItem {
-		
-		@Getter @Setter 
+
+		@Getter
+		@Setter
 		private String title;
-		
-		@Getter @Setter 
+
+		@Getter
+		@Setter
 		private String body;
-		
-		@Getter @Setter 
+
+		@Getter
+		@Setter
 		private String createdBy;
-		
-		@Getter @Setter 
+
+		@Getter
+		@Setter
 		private Date createdOn;
-		
-		@Getter @Setter 
+
+		@Getter
+		@Setter
 		private String channel;
 	}
 
