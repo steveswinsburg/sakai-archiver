6226070a7c14fa602cbcf89c7d541e046c3b6801
Amelia Davis <amiedav@gmail.com>
2017-06-12 20:11:46 +1000

    #19 Set up basic archiving of forums
    Attachments archiving is a WIP
    Need to change what is archived depending on whether includeStudentContent is true.

--- provider-forums/src/java/org/sakaiproject/archiver/provider/ForumsArchiver.java
+++ provider-forums/src/java/org/sakaiproject/archiver/provider/ForumsArchiver.java
@@ -1,9 +1,12 @@
 package org.sakaiproject.archiver.provider;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.sakaiproject.api.app.messageforums.DiscussionForum;
+import org.sakaiproject.api.app.messageforums.DiscussionTopic;
+import org.sakaiproject.api.app.messageforums.Message;
 import org.sakaiproject.api.app.messageforums.ui.DiscussionForumManager;
 import org.sakaiproject.archiver.api.ArchiverRegistry;
 import org.sakaiproject.archiver.api.ArchiverService;
@@ -16,6 +19,7 @@ import org.sakaiproject.exception.PermissionException;
 import org.sakaiproject.exception.ServerOverloadException;
 import org.sakaiproject.exception.TypeException;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -48,17 +52,84 @@ public class ForumsArchiver implements Archiveable {
 	@Override
 	public void archive(final String archiveId, final String siteId, final String toolId, final boolean includeStudentContent) {
 
-		List<DiscussionForum> forums = this.forumManager.getDiscussionForumsByContextId(siteId);
+		List<DiscussionForum> forums = this.forumManager.getDiscussionForumsWithTopics(siteId);
 		
 		for (DiscussionForum forum : forums) {
 			
 			SimpleForum simpleForum = new SimpleForum(forum);
-			this.archiverService.archiveContent(archiveId, siteId, toolId, Jsonifier.toJson(simpleForum).getBytes(), forum.getTitle() + ".json");
+			this.archiverService.archiveContent(archiveId, siteId, toolId, Jsonifier.toJson(simpleForum).getBytes(), forum.getTitle() + ".json", forum.getTitle());
 			
-			this.archiveAttachments(forum, archiveId, siteId, toolId);
+			// TODO: Different with student content
+			//this.archiveAttachments(forum, archiveId, siteId, toolId);
+			
+			this.archiveTopics(forum, archiveId, siteId, toolId);
 			
 		}
+	}
+	
+	/**
+	 * Archive the topics within a forum
+	 * 
+	 * @param forum
+	 * @param archiveId
+	 * @param siteId
+	 * @param toolId
+	 */
+	private void archiveTopics(DiscussionForum forum, final String archiveId, final String siteId, final String toolId) {
 		
+		List<DiscussionTopic> topics = forum.getTopics();
+		for (DiscussionTopic topic : topics) {
+			SimpleTopic simpleTopic = new SimpleTopic(topic);
+			this.archiverService.archiveContent(archiveId, siteId, toolId, Jsonifier.toJson(simpleTopic).getBytes(), 
+					"topic-metadata.json", forum.getTitle() + "/topics/" + topic.getTitle());
+			
+			//TODO: Topic attachments
+			
+			// Archive the messages within a topic
+			String folderStructure = forum.getTitle() + "/topics/" + topic.getTitle();
+			 
+			List<Message> messages = topic.getMessages();
+			for (Message message : messages) {
+				
+				// Find the top level message
+				if (message.getInReplyTo() == null) {
+					SimpleMessage topLevelMessage = new SimpleMessage(message);
+					// Set message replies
+					this.setMessageReplies(topLevelMessage, message, messages);
+					
+					// Archive the messages
+					this.archiverService.archiveContent(archiveId, siteId, toolId, Jsonifier.toJson(topLevelMessage).getBytes(), 
+							message.getTitle() + ".json", folderStructure);
+				}
+				
+				//TODO: Message attachments
+				if (message.getHasAttachments()) {
+					
+				}
+			}
+		}
+		
+	}
+	
+	/**
+	 * Add any replies to each SimpleMessage object
+	 * 
+	 * @param simpleTopMessage
+	 * @param topMessage
+	 * @param messages
+	 */
+	private void setMessageReplies(SimpleMessage simpleTopMessage, Message topMessage, List<Message> messages) {
+		
+		for (Message message : messages) {
+			if (message.getInReplyTo() == topMessage) {
+				List<SimpleMessage> replies = simpleTopMessage.getReplies();
+				SimpleMessage thisMessage = new SimpleMessage(message);
+				replies.add(thisMessage);
+				
+				// Recursively set the replies for this inner message
+				this.setMessageReplies(thisMessage, message, messages);
+			}
+		}
 	}
 	
 	/**
@@ -99,9 +170,129 @@ public class ForumsArchiver implements Archiveable {
 		this.archiverService.archiveContent(archiveId, siteId, toolId, attachmentBytes, 
 				attachment.getProperties().getPropertyFormatted(attachment.getProperties().getNamePropDisplayName()), subdir);
 	}
+	
+	/**
+	 *  Simplified Message class
+	 */
+	private class SimpleMessage {
 
+		@Setter
+		private Long messageId;
+		
+		@Setter
+		private String title;
+		
+		@Setter
+		private String body;
+		
+		@Setter
+		private Date lastModified;
+		
+		@Setter
+		private String authoredBy;
+		
+		@Setter
+		private String authorId;
+		
+		@Setter
+		private Long replyTo;
+		
+		@Setter
+		private Date createdOn;
+		
+		@Setter
+		private boolean isDraft;
+		
+		@Setter
+		private boolean isDeleted;
+
+		@Setter
+		private String modifiedBy;
+		
+		@Setter @Getter
+		private List<SimpleMessage> replies = new ArrayList<SimpleMessage>();
+		
+		public SimpleMessage(Message message) {
+			
+			this.messageId = message.getId();
+			this.title = message.getTitle();
+			this.body = message.getBody();
+			this.lastModified = message.getModified();
+			this.authoredBy = message.getAuthor();
+			this.authorId = message.getAuthorId();
+			this.isDraft = message.getDraft();
+			this.isDeleted = message.getDeleted();
+			this.createdOn = message.getCreated();
+			this.modifiedBy = message.getModifiedBy();
+			
+			Message parent = message.getInReplyTo();
+			if (parent != null) {
+				this.replyTo = parent.getId();
+			} 
+		}
+	}
 
 	
+	/**
+	 * Simplified Topic class
+	 */
+	private class SimpleTopic {
+		
+		@Setter
+		private Long topicId;
+		
+		@Setter
+		private String title;
+		
+		@Setter
+		private Date createdDate;
+		
+		@Setter
+		private String creator;
+		
+		@Setter
+		private Date modifiedDate;
+		
+		@Setter
+		private String modifier;
+		
+		@Setter
+		private Boolean isLocked;
+
+		@Setter
+		private Boolean isPostFirst;
+
+		@Setter
+		private String assocGradebookItemName;
+
+		@Setter
+        private Date openDate;
+
+		@Setter
+        private Date closeDate;
+	        
+		public SimpleTopic (DiscussionTopic topic) {
+			this.topicId = topic.getId();
+			this.title = topic.getTitle();
+			this.createdDate = topic.getCreated();
+			this.creator = topic.getCreatedBy();
+			this.modifiedDate = topic.getModified();
+			this.modifier = topic.getModifiedBy();
+			this.isLocked = topic.getLocked();
+			this.isPostFirst = topic.getPostFirst();
+			this.assocGradebookItemName = topic.getDefaultAssignName();
+			
+			// if availability is restricted, get open and close dates
+			if (topic.getAvailabilityRestricted()) {
+				this.openDate = topic.getOpenDate();
+				this.closeDate = topic.getCloseDate();
+			}
+		}
+	}
+
+	/**
+	 * Simplified Forum task
+	 */
 	private class SimpleForum {
 		
 		@Setter
@@ -166,8 +357,5 @@ public class ForumsArchiver implements Archiveable {
 				this.closeDate = forum.getCloseDate();
 			}
 		}
-		
 	}
-	
-
 }
